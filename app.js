320e800c326860ea7a859654d607e00d1e917147
codemiz <ik571572@gmail.com>
2025-07-12 00:26:40 +0500

    deleted file

--- app.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-const key = JSON.parse(fs.readFileSync('./serviceAccountKey.json', 'utf8'));
-
-// this will escape newlines properly
-const escapedKey = key.private_key.replace(/\n/g, '\\n');
-
-console.log(JSON.stringify(escapedKey)); // use this to see exact escaped output
-
